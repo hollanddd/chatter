ea6fdd26875f46b3e336268e3cdf1ce353fce7d7
hollanddd <me@darrenholland.com>
2012-12-11 15:26:00 -0600

    passing around done() gives false positive aka bullshit

--- test/test.js
+++ test/test.js
@@ -5,54 +5,71 @@ var socketURL = 'http://127.0.0.1:1337';
 
 var options = { transports: ['websocket'], 'force new connection' : true };
 
-var user1 = {'nick': 'Darren'};
-var user2 = {'nick': 'Jerry'};
+var user_one = {'nick': 'Darren'}, user_two = {'nick': 'Jerry'};
 
 describe("Chat Server", function(){
   
   it('Should broadcast a new user when they connect', function(done){
     var client = io.connect(socketURL, options);
     client.on('connect', function(data){
-      client.emit('set nick', user1);
+      client.emit('set nick', user_one);
     });
 
     client.on('broadcast_msg', function(user_nick){
       user_nick.should.be.a('string');
-      user_nick.should.equal(user1.nick + ' is now connected.');
+      user_nick.should.equal(user_one.nick + ' is now connected.');
+      client.disconnect();
     });
     done();
   });//end should not blow up
   
   it('Should broadcast new users to all users', function(done){
-    var socket1 = io.connect(socketURL, options);
+    var client_one = io.connect(socketURL, options);
 
-    socket1.on('connect', function(data){
-      socket1.emit('set nick', user1.nick);
+    client_one.on('connect', function(data){
+      client_one.emit('set nick', user_one.nick);
 
       //connect the second socket
-      var socket2 = io.connect(socketURL, options);
+      var client_two = io.connect(socketURL, options);
 
-      socket2.on('connect', function(data){
-        socket2.emit('set nick', user2.nick);
+      client_two.on('connect', function(data){
+        client_two.emit('set nick', user_two.nick);
       });
 
-      socket2.on('nick', function(nick){
-        nick.should.equal(user2.nick);
-        client2.disconnect();
+      client_two.on('nick', function(nick){
+        nick.should.equal(user_two.nick);
+        client_one.disconnect();
       });
     });
     
     var numUsers = 0; 
-    socket1.on('new user', function(nick){
+    client_one.on('new user', function(nick){
       numUsers += 1;
 
       if(numUsers === 2){
-        nick.should.equal(user2.nick + " has joined.");
-        socket1.disconnect();
+        nick.should.equal(user_two.nick + " has joined.");
+        client_one.disconnect();
         done();
       }
     });
-    done();
-  });//end should broadcast new user 
+  });//end should broadcast new user
+
+  it('should broadcast a message to all users', function(done){
+    var client_one = io.connect(socketURL, options);
+    var msg = 'hello world';
+
+    client_one.on('connect', function(data){
+      client_one.emit('set nick', user_one);
+      
+      var client_two = io.connect(socketURL, options);
+      client_two.on('connect', function(data){
+        client_two.emit('set nick', user_two);
+        client_two.emit('emit msg', msg);
+        client_two.on('broadcast_msg', function(message){
+          message.should.equal(user_two.nick + ':' + msg);
+        });
+      });
+    });
+  });//end should broadcast a message to all users
 
 });//end describe chat server
